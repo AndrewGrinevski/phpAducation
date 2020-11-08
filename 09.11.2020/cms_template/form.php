d4f3c1a5858d74835f9b4ffcb30ce0d11b59979d
Andrew Grinewski <ermaking1@gmail.com>
2020-11-08 20:05:47 +0300

    Add project Hm6

---
+++ 09.11.2020/cms_template/form.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+<form action="handler.php" method="post">
+    <h3>Введите текст:</h3>
+    <input type="text" placeholder="Текст" name="randomText" class="text">
+    <h3>Выберите № функции:</h3>
+    <input type="text" placeholder="1" name="numberOfFunction" class="functions">
+    <button type="submit">Отправить</button>
+    <div class="description">
+        <h3>Описание функций:</h3>
+        <p>1) Убирает пробелы в тексте.</p>
+        <p>2) Выводит на экран все слова длина которых не менее трёх символов и не более пяти.</p>
+        <p>3) Является ли текст палиндромом.</p>
+        <p>4) Находит самое длинное слово и выводит его на экран. Случай, когда самых длинных слов может быть несколько,
+            не обрабатывает.
+        </p>
+        <p>5) Считает количество строчных и прописных букв в строке.</p>
+    </div>
+</form>
---
+++ 09.11.2020/cms_template/handler.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+
+<h3>Ответ:</h3>
+<?php
+include_once 'functions.php';
+?>
+<p style="color: #0c5460">
+    <?php
+    if ($_POST['numberOfFunction'] == 1) {
+        echo firstTask($_POST['randomText']);
+    } elseif ($_POST['numberOfFunction'] == 2) {
+        print_r(secondTask($_POST['randomText']));
+    } elseif ($_POST['numberOfFunction'] == 3) {
+        echo thirdTask($_POST['randomText']);
+    } elseif ($_POST['numberOfFunction'] == 4) {
+        echo fourTask($_POST['randomText']);
+    } elseif ($_POST['numberOfFunction'] == 5) {
+        print_r(fiveTask($_POST['randomText']));
+    } elseif ($_POST['numberOfFunction'] > 5) {
+        echo "ПОТРАЧЕНО";
+    }
+    ?>
+</p>
+
