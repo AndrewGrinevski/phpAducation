27dcac91ff91318b3d3d0987842019cceec8f97d
Andrew Grinewski <ermaking1@gmail.com>
2020-11-22 18:12:43 +0300

    Add project Hm 8

---
+++ 22.11.2020/admin/index_admin.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/' . $name . '_header.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="post-preview">
+                    <a href="/?page_type=../form">
+                        <h2 class="post-title">
+                            Add article
+                        </h2>
+                    </a>
+                    <img src="https://kor.ill.in.ua/m/610x0/1923462.jpg?v=636171670238630514" alt="">
+                    <h3 class="post-subtitle">
+                        Made new information...
+                    </h3>
+                </div>
+                <hr>
+        </div>
+    </div>
+</div>
